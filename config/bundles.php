e4619720755745ff7ce6da65e400347ad6a2d543
Tobias Schlemmer <github@tsc-web.net>
2017-11-13 02:39:55 +0100

    Downgrade to php70 and composer update.

--- config/bundles.php
+++ config/bundles.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    'Symfony\Bundle\FrameworkBundle\FrameworkBundle' => ['all' => true],
-    'Symfony\Bundle\WebServerBundle\WebServerBundle' => ['dev' => true],
-    'Symfony\Bundle\TwigBundle\TwigBundle' => ['all' => true],
-    'Symfony\Bundle\SecurityBundle\SecurityBundle' => ['all' => true],
-    'Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle' => ['all' => true],
-    'Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle' => ['all' => true],
-    'Doctrine\Bundle\DoctrineBundle\DoctrineBundle' => ['all' => true],
-    'JavierEguiluz\Bundle\EasyAdminBundle\EasyAdminBundle' => ['all' => true],
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    JavierEguiluz\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
